aa647613d2ada1ec10ad2093ad2f34d6fc9e26f1
bkp506 <qualeetee114@gmail.com>
2018-03-25 23:22:01 +0530

    my 2nd git commit

---
+++ com.testSelenium.DemoMaven/src/test/java/GlobalMethod1/FirefoxOptions.java
@@ -0,0 +1,14 @@
+package GlobalMethod1;
+
+import org.openqa.selenium.firefox.FirefoxBinary;
+
+public class FirefoxOptions {
+
+	public static final String FIREFOX_OPTIONS = null;
+
+	public String setBinary(FirefoxBinary firefoxBinary) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
